ecfadf18445737570d521cefeadd4f79c0667593
Abid <ahmedabid3409@gmail.com>
2022-04-28 04:13:06 +0600

    Edit section complete

--- routes/article.js
+++ routes/article.js
@@ -6,6 +6,26 @@ article.get("/new",(req,res)=>{
 res.render("articles/new");
 });
 
+article.get("/edit",async(req,res)=>{
+    _id=req.query._id;
+    const article1=await mongoose.model("Article").findById({_id:_id});
+    //console.log(article1);
+
+    await res.render("articles/editArticle",{article:article1});
+    });
+    article.post("/editArticle",async(req,res)=>{
+        _id=req.query._id;
+        console.log(_id);
+        const title=req.body.title;
+        const des=req.body.des;
+        const article1=await mongoose.model("Article").findByIdAndUpdate({_id:_id},{
+title:title,
+des:des
+        });
+    
+        await res.redirect("/");
+        }); 
+
 article.get("/delete",async(req,res)=>{
    let  _id=req.query._id;
     console.log(_id);
@@ -19,7 +39,7 @@ article.get("/:id",async (req,res)=>{
      let date=new Date();
      let month=date.getMonth();
     const recent=await mongoose.model("Article").find({month:month});
-    console.log(recent);
+   // console.log(recent);
    // const art=JSON.parse(article)
     await res.render("articles/showArticle",{article:article,recent:recent});
 })
